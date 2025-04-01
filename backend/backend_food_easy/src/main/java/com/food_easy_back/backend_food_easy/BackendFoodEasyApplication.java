11c4a992d7c2d91dd75f4a527a758ecd825060bc
charlie9710 <carlos.encina.dev@gmail.com>
2025-04-01 20:54:33 -0300

    Creacion de proyecto spring

---
+++ backend/backend_food_easy/src/main/java/com/food_easy_back/backend_food_easy/BackendFoodEasyApplication.java
@@ -0,0 +1,13 @@
+package com.food_easy_back.backend_food_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFoodEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFoodEasyApplication.class, args);
+	}
+
+}
---
+++ backend/backend_food_easy/src/test/java/com/food_easy_back/backend_food_easy/BackendFoodEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_easy_back.backend_food_easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFoodEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
