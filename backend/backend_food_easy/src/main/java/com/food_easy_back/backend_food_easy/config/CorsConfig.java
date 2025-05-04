72c2254bfb455058775be6e4f50cfb22608a0a6a
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2025-05-04 12:59:54 -0400

    cambios en configuracion de cors

--- backend/backend_food_easy/src/main/java/com/food_easy_back/backend_food_easy/config/CorsConfig.java
+++ backend/backend_food_easy/src/main/java/com/food_easy_back/backend_food_easy/config/CorsConfig.java
@@ -1,3 +1,32 @@
+// package com.food_easy_back.backend_food_easy.config;
+
+// import java.util.Arrays;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.CorsConfigurationSource;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+// @Configuration
+// public class CorsConfig {
+
+//     @Bean
+//     CorsConfigurationSource corsConfigurationSource(){
+//         CorsConfiguration corsConfiguration = new CorsConfiguration();
+//         corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+//         corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+//         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept","*"));
+//         corsConfiguration.setAllowCredentials(true);
+
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+//         source.registerCorsConfiguration("/**", corsConfiguration);
+
+//         return source;
+//     }
+
+// }
 package com.food_easy_back.backend_food_easy.config;
 
 import java.util.Arrays;
@@ -12,17 +41,18 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class CorsConfig {
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource(){
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173/"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization")); // ← permite leer el token desde el frontend
+        corsConfiguration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
         source.registerCorsConfiguration("/**", corsConfiguration);
 
         return source;
     }
-
 }
+
