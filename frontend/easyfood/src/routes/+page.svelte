b0fc470c40cd1878a18466cef249b25e71ed6a18
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2025-05-07 11:35:59 -0400

    se agrega img de fondo en pagina login

--- frontend/easyfood/src/routes/+page.svelte
+++ frontend/easyfood/src/routes/+page.svelte
@@ -24,14 +24,17 @@
 	}
 </script>
 
-<div class="container vh-100 d-flex justify-content-center align-items-center">
-	<div class="" style="max-width: 500px;">
-		<div class="text-center mb-4">
-			<h1>Bienvenido a Easy Food</h1>
-			<p>Administra tu inventario de productos de manera sencilla.</p>
-		</div>
+<div class="inicio vh-100 d-flex justify-content-center align-items-center">
+	<div class="container" style="max-width: 500px;">
+
 		<div class="card shadow">
+			
 			<div class="card-body">
+
+				<div class="text-center mb-4">
+					<h1>Bienvenido a Easy Food</h1>
+					<p>Administra tu inventario de productos de manera sencilla.</p>
+				</div>
 				<form on:submit|preventDefault={() => handleLogin(username, password)}>
 					<div class="mb-3">
 						<label for="username" class="form-label">Usuario</label>
@@ -58,3 +61,13 @@
 		</div>
 	</div>
 </div>
+
+<style>
+.inicio{
+	background-image: url("/img/fondo-easyfood.png");
+	/* background-position: left bottom; */
+	background-repeat: no-repeat;
+}
+
+
+</style>
